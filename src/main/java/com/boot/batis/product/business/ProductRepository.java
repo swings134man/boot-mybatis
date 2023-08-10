fed92d556f3946f6245daac5cc8c9267eb98a9b6
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2023-08-10 21:19:34 +0900

    커밋 정리

--- src/main/java/com/boot/batis/product/business/ProductRepository.java
+++ src/main/java/com/boot/batis/product/business/ProductRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-//@Mapper
+@Mapper // MyBatis Config에서 Base Packge 설정 시 생략 가능
 public interface ProductRepository {
 
     void save(ProductDTO dto);
